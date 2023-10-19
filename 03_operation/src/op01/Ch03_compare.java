7e82176b8ce09f330b581a45eb7bd69571760048
kimhyejeongd <heajung2665@naver.com>
2023-10-19 17:55:32 +0900

    2일째 마지막

---
+++ 03_operation/src/op01/Ch03_compare.java
@@ -0,0 +1,17 @@
+package op01;
+
+public class Ch03_compare {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 50;
+		boolean re1 = num1 == num2;
+		
+		
+		System.out.println("num1과 num2가 값이 같은가? " + (num1==num2));//(num1==num2) 또는 re2
+		
+		System.out.println("num1과 num2가 같지 않은가? " + (num1 != num2));
+
+	}
+
+}
